3ffa949c7ae9c15b62f3aee71097a69b4d94a4cb
Vimal Anand <78069624+Vimal2023@users.noreply.github.com>
2025-03-28 00:11:19 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <div className='heading'>
-      <h1 className=''>3x3 Matrix Game</h1>
+      <h1 className=''>3x3 Matrix Game <a href='https://github.com/Vimal2023/3x3-Matrix-Game'target="_blank" rel="noreferrer"><span className='me'>(CODE)</span></a></h1>
       <h2 className='second'>develop by <a href='https://portfolio-c-vimal-anand.vercel.app/' target="_blank" rel="noreferrer"><span className='me' >@Vimal-Anand</span></a></h2>
       </div>
       <Matrix />
